c17a015b5223aa738537003126435a7f272fb085
Patrick Faria <patrickarfaria@gmail.com>
2019-05-09 21:30:36 -0300

    Produto para associação muitos para muitos

--- src/main/java/com/patrick/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/patrick/cursomc/repositories/ProdutoRepository.java
@@ -3,12 +3,11 @@ package com.patrick.cursomc.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.patrick.cursomc.domain.Categoria;
+import com.patrick.cursomc.domain.Produto;
 
 @Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
 
-	
 	
 	
 }

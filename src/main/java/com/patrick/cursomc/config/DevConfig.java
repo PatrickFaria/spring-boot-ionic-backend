9f40f65c92ba024f55b586d7ed89b086dff7795b
Patrick Faria <patrickarfaria@gmail.com>
2019-06-13 19:49:53 -0300

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/com/patrick/cursomc/config/DevConfig.java
+++ src/main/java/com/patrick/cursomc/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.patrick.cursomc.services.DBService;
+import com.patrick.cursomc.services.EmailService;
+import com.patrick.cursomc.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -30,6 +32,11 @@ public class DevConfig {
 		
 		
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 	
 }
--- src/main/java/com/patrick/cursomc/services/DBService.java
+++ src/main/java/com/patrick/cursomc/services/DBService.java
@@ -122,7 +122,7 @@ public class DBService {
 		estadoRepository.saveAll(Arrays.asList(est1,est2));
 		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
 		
-		Cliente cli1 = new Cliente(null,"Maria Silva","maria@gmail.com","33333333333",TipoCliente.PESSOAFISICA);
+		Cliente cli1 = new Cliente(null,"Maria Silva","patrickarfaria@gmail.com","33333333333",TipoCliente.PESSOAFISICA);
 		cli1.getTelefones().addAll(Arrays.asList("2312312546","3545234523"));
 		
 		

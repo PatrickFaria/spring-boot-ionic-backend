c9268b50826aef6c6984f6416396794a4a159a88
Patrick Faria <patrickarfaria@gmail.com>
2019-06-25 20:51:39 -0300

    Restrição de conteúdo: cliente só recupera seus pedidos

--- src/main/java/com/patrick/cursomc/services/PedidoService.java
+++ src/main/java/com/patrick/cursomc/services/PedidoService.java
@@ -4,8 +4,12 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.patrick.cursomc.domain.Cliente;
 import com.patrick.cursomc.domain.ItemPedido;
 import com.patrick.cursomc.domain.PagamentoComBoleto;
 import com.patrick.cursomc.domain.Pedido;
@@ -13,39 +17,42 @@ import com.patrick.cursomc.domain.enums.EstadoPagamento;
 import com.patrick.cursomc.repositories.ItemPedidoRepository;
 import com.patrick.cursomc.repositories.PagamentoRepository;
 import com.patrick.cursomc.repositories.PedidoRepository;
+import com.patrick.cursomc.security.UserSS;
+import com.patrick.cursomc.services.exceptions.AuthorizationException;
 import com.patrick.cursomc.services.exceptions.ObjectNotFoundException;
+
+
 
 @Service
 public class PedidoService {
-	
+
 	@Autowired
 	private PedidoRepository repo;
-	
+
 	@Autowired
 	private BoletoService boletoService;
-	
+
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
-	
-	@Autowired
-	private ProdutoService produtoService;
-	
+
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
-	
+
+	@Autowired
+	private ProdutoService produtoService;
+
 	@Autowired
 	private ClienteService clienteService;
-	
+
 	@Autowired
 	private EmailService emailService;
-	
+
 	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
 	}
-	
-	
+
 	public Pedido insert(Pedido obj) {
 		obj.setId(null);
 		obj.setInstante(new Date());
@@ -65,8 +72,20 @@ public class PedidoService {
 			ip.setPedido(obj);
 		}
 		itemPedidoRepository.saveAll(obj.getItems());
-		emailService.sendOrderConfirmationHtmlEmail(obj);
+		emailService.sendOrderConfirmationEmail(obj);
 		return obj;
 	}
+
 	
+
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente =  clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
+	}
+
 }

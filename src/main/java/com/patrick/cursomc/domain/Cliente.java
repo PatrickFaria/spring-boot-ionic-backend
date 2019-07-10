490ab962718590dd94d368a54e6d4487150e153b
Patrick Faria <patrickarfaria@gmail.com>
2019-07-10 20:43:47 -0300

    Usando padrão de nomes para imagens

--- src/main/java/com/patrick/cursomc/domain/Cliente.java
+++ src/main/java/com/patrick/cursomc/domain/Cliente.java
@@ -56,8 +56,6 @@ public class Cliente implements Serializable{
 	@OneToMany(mappedBy="cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 	
-	private String imageURL;
-	
 	public Cliente() {
 		addPerfil(Perfil.CLIENTE);
 	}
@@ -183,13 +181,6 @@ public class Cliente implements Serializable{
 		return true;
 	}
 
-	public String getImageURL() {
-		return imageURL;
-	}
-
-	public void setImageURL(String imageURL) {
-		this.imageURL = imageURL;
-	}
 
 
 	

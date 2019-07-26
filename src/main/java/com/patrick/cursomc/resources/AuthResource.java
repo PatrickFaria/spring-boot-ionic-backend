c27ae26210540aff8c199dfc5cd2b58ab6bb8650
Patrick Faria <patrickarfaria@gmail.com>
2019-07-26 15:21:37 -0300

    Correção erro de Cors

--- src/main/java/com/patrick/cursomc/resources/AuthResource.java
+++ src/main/java/com/patrick/cursomc/resources/AuthResource.java
@@ -3,6 +3,8 @@ package com.patrick.cursomc.resources;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,16 +18,16 @@ import com.patrick.cursomc.security.UserSS;
 import com.patrick.cursomc.services.AuthService;
 import com.patrick.cursomc.services.UserService;
 
+
 @RestController
 @RequestMapping(value = "/auth")
 public class AuthResource {
-
 	@Autowired
 	private JWTUtil jwtUtil;
 	
 	@Autowired
 	private AuthService service;
-	
+
 	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
 	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
 		UserSS user = UserService.authenticated();
@@ -40,5 +42,5 @@ public class AuthResource {
 		service.sendNewPassword(objDto.getEmail());
 		return ResponseEntity.noContent().build();
 	}
-	
+
 }
